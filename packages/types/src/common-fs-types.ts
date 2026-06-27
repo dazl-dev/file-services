931f4c76439da04d2589c79f21d745fd64cd2410
Avi Vahl <avi@vahl.co.il>
2026-06-27 16:24:24 +0300

    chore(deps-dev): prettier@3.9.0 and regenerate lock file
    https://github.com/prettier/prettier/releases/tag/3.9.0

--- packages/types/src/common-fs-types.ts
+++ packages/types/src/common-fs-types.ts
@@ -1,14 +1,5 @@
 export type BufferEncoding =
-  | "ascii"
-  | "utf8"
-  | "utf-8"
-  | "utf16le"
-  | "ucs2"
-  | "ucs-2"
-  | "base64"
-  | "latin1"
-  | "binary"
-  | "hex";
+  "ascii" | "utf8" | "utf-8" | "utf16le" | "ucs2" | "ucs-2" | "base64" | "latin1" | "binary" | "hex";
 
 export interface StatSyncOptions {
   /**
